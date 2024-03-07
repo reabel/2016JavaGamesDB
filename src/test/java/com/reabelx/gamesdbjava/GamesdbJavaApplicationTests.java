832b41a483e2dfcab7b0b0322309ed3034970da6
reabel <ryanreabel@gmail.com>
2024-03-07 18:44:41 -0500

    Basic Springboot app commit

---
+++ src/test/java/com/reabelx/gamesdbjava/GamesdbJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reabelx.gamesdbjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesdbJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
